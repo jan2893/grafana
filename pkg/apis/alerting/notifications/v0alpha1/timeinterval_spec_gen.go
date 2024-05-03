3f0e32187fbef04be674fbbd9ce399b3aca8a2a0
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2024-05-03 13:36:30 -0400

    temp

---
+++ pkg/apis/alerting/notifications/v0alpha1/timeinterval_spec_gen.go
@@ -0,0 +1,25 @@
+package v0alpha1
+
+// TimeIntervalInterval defines model for TimeIntervalInterval.
+// +k8s:openapi-gen=true
+type TimeIntervalInterval struct {
+	DaysOfMonth []string                `json:"daysOfMonth,omitempty"`
+	Location    *string                 `json:"location,omitempty"`
+	Months      []string                `json:"months,omitempty"`
+	Times       []TimeIntervalTimeRange `json:"times,omitempty"`
+	Weekdays    []string                `json:"weekdays,omitempty"`
+	Years       []string                `json:"years,omitempty"`
+}
+
+// TimeIntervalSpec defines model for TimeIntervalSpec.
+// +k8s:openapi-gen=true
+type TimeIntervalSpec struct {
+	Intervals []TimeIntervalInterval `json:"intervals"`
+}
+
+// TimeIntervalTimeRange defines model for TimeIntervalTimeRange.
+// +k8s:openapi-gen=true
+type TimeIntervalTimeRange struct {
+	EndMinute   string `json:"endMinute"`
+	StartMinute string `json:"startMinute"`
+}
